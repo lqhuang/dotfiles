94e78a057bbbdfa95c9ef08af42c773d5fc5f360
Lanqing Huang <lqhuang@outlook.com>
2023-02-18 22:25:52 +0800

    Add script to install vscode-cli

---
+++ portable-bin/install-code-cli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Reference and inspired
+# https://aur.archlinux.org/packages/visual-studio-code-bin
+
+set -eu -o pipefail
+
+# deps required by vscode for Arch / Manjaro
+# (at-spi2-core alsa-lib gtk3 libcups)
+
+# APP=vscode-linux-x64
+# SOURCE_URL=https://code.visualstudio.com/sha/download?build=stable&os=linux-x64
+# FNAME=code-stable-x64.tar.gz
+# TARGET_DIR=${HOME}/Downloads
+
+APP=vscode-linux-cli-x64
+SOURCE_URL=https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64
+FNAME=vscode-cli-alpine-x64-cli.tar.gz
+TARGET_DIR=${HOME}/Downloads
+INSTALL_DIR=${HOME}/.local/bin
+
+mkdir -p "${TARGET_DIR}"
+curl -fSL "${SOURCE_URL}" -o "${TARGET_DIR}/${FNAME}"
+
+mkdir -p "${INSTALL_DIR}"
+tar -xzf "${TARGET_DIR}/${FNAME}" -C "${INSTALL_DIR}"
+# --overwrite
+# -k, --keep-old-files
