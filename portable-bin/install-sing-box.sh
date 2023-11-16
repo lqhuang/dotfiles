0af12bef5f624d877d714900b2788f5567703971
Lanqing Huang <lqhuang@outlook.com>
2023-11-16 11:07:55 +0800

    Fix incorrect postprecess

--- portable-bin/install-sing-box.sh
+++ portable-bin/install-sing-box.sh
@@ -9,8 +9,8 @@ VERSION="1.6.4"
 
 REPO=SagerNet/sing-box
 NAME=sing-box
-RAW_CLI_NAME="${NAME}-${VERSION}-${DISTRO}-${ARCH}" # example: sing-box-1.6.4-linux-amd64v3
-ASSET="${RAW_CLI_NAME}.tar.gz"
+DIST_DIR="${NAME}-${VERSION}-${DISTRO}-${ARCH}" # example: sing-box-1.6.4-linux-amd64v3
+ASSET="${DIST_DIR}.tar.gz"
 
 # example url:
 # https://github.com/SagerNet/sing-box/releases/download/v1.6.4/sing-box-1.6.4-linux-amd64v3.tar.gz
@@ -23,8 +23,8 @@ echo "${REPO} ${VERSION}: Downloading from ${VERSIONED_URL} and extracting binar
 mkdir -p ${TARGET_DIR}
 curl -L ${VERSIONED_URL} | tar -C ${TARGET_DIR} -xzf -
 
-mv ${TARGET_DIR}/${RAW_CLI_NAME} ${TARGET_DIR}/${NAME}
-chmod u+x ${TARGET_DIR}/${NAME}
+mv "${TARGET_DIR}/${DIST_DIR}/${NAME}" "${TARGET_DIR}/"
+rm -rf "${TARGET_DIR}/${DIST_DIR}"
 
 echo "Testing installation of ${NAME}"
 command -v ${NAME}
