b542c7a3c5c77fc46eb740ed1deb46b9d9c9fe52
Lanqing Huang <lqhuang@outlook.com>
2024-06-11 16:44:45 +0800

    Reorg script to install code-cli

--- vscode/install-code-cli.sh
+++ vscode/install-code-cli.sh
@@ -7,15 +7,11 @@ set -eu -o pipefail
 # deps required by vscode for Arch / Manjaro
 # (at-spi2-core alsa-lib gtk3 libcups)
 
-# APP=vscode-linux-x64
-# SOURCE_URL=https://code.visualstudio.com/sha/download?build=stable&os=linux-x64
-# FNAME=code-stable-x64.tar.gz
-# TARGET_DIR=${HOME}/Downloads
-
 APP="vscode-linux-cli-x64"
 SOURCE_URL="https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64"
 FNAME="vscode-cli-alpine-x64-cli.tar.gz"
-TARGET_DIR="${HOME}/Downloads"
+# TARGET_DIR=${HOME}/Downloads
+TARGET_DIR="/tmp"
 INSTALL_DIR="${HOME}/.local/bin"
 
 mkdir -p "${TARGET_DIR}"
@@ -23,5 +19,6 @@ curl -fSL "${SOURCE_URL}" -o "${TARGET_DIR}/${FNAME}"
 
 mkdir -p "${INSTALL_DIR}"
 tar -xzf "${TARGET_DIR}/${FNAME}" -C "${INSTALL_DIR}"
+
 # --overwrite
 # -k, --keep-old-files
