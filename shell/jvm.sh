aaaf9ffaff1188a24429f708499d3f413949ab28
Lanqing Huang <lqhuang@outlook.com>
2025-07-24 21:22:05 +0800

    Disable `MILL_OUTPUT_DIR` temporarily

--- shell/jvm.sh
+++ shell/jvm.sh
@@ -5,7 +5,11 @@
 # sbt / scala / sdkman
 export SBT_OPTS="-Dsbt.override.build.repos=true ${SBT_OPTS}"
 export JVM_OPTS="-Dhttps.protocols=TLSv1.2,TLSv1.3 ${JVM_OPTS}"
-export MILL_OUTPUT_DIR=".mill"
+
+# export MILL_OUTPUT_DIR=".mill"
+# Unsupported while mill >= v1.0.0 ???
+# Cause error => generatedScriptSources java.io.IOException Is a directory
+# not fixed yet. deprecate temporarily and fallback to defaults instead
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="${HOME}/.sdkman"
