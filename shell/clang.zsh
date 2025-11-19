60430a7e6b9a07d6e7be3e2699cc66eed2328ee7
Lanqing Huang <lqhuang@outlook.com>
2025-11-19 21:05:23 +0800

    chore: Append missing `clang.zsh`

--- shell/clang.zsh
+++ shell/clang.zsh
@@ -2,14 +2,15 @@ KERNEL_NAME=$(uname -s)  # Linux / Darwin
 SHELL_NAME="${SHELL##*/}"  # zsh / bash
 
 HOMEBREW_LLVM="${HOMEBREW_PREFIX}/opt/llvm"
-
-if [[ -d ${HOMEBREW_LLVM} ]]; then
-  export PATH="${HOMEBREW_LLVM}/bin:$PATH"
+if [[ -d "${HOMEBREW_LLVM}" ]]; then
+  export PATH="${HOMEBREW_LLVM}/bin:${PATH}"
   export LDFLAGS="-L${HOMEBREW_LLVM}/lib/c++ -L${HOMEBREW_LLVM}/lib/unwind -lunwind"
 fi
 
-
+# NOTE:
 # To install symlinks for compilers that will automatically use
 # ccache, prepend this directory to your PATH:
-#   /opt/homebrew/opt/ccache/libexec
-
+HOMEBREW_CCACHE="${HOMEBREW_PREFIX}/opt/ccache"
+if [[ -d "${HOMEBREW_CCACHE}" ]]; then
+  export PATH="${HOMEBREW_CCACHE}/libexec:${PATH}"
+fi
