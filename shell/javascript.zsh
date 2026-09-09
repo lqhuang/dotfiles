de862b0aff333545c21210faef4cd7fee9b3e095
Lanqing Huang <lqhuang@outlook.com>
2026-09-09 12:39:10 +0800

    Disable `fnm` in shell init stage

--- shell/javascript.zsh
+++ shell/javascript.zsh
@@ -5,41 +5,42 @@ SHELL_NAME="${SHELL##*/}"  # zsh / bash
 # Javascript / Typescript
 #
 
-# fnm
-if [[ ${KERNEL_NAME} == "Linux" ]]; then
-  FNM_DIR="${HOME}/.local/share/fnm"
-  if [[ -d "${FNM_DIR}" ]]; then
-    export PATH="${FNM_DIR}:${PATH}"
-  fi
-fi
-if [[ -s $(command -v fnm) ]]; then
-  eval "$(fnm env)"
-  # eval "$(fnm env --use-on-cd)"
-  # completions are registered by brew already
-  #source <(fnm completions --shell ${SHELL_NAME})
+## fnm
+# if [[ ${KERNEL_NAME} == "Linux" ]]; then
+#   FNM_DIR="${HOME}/.local/share/fnm"
+#   if [[ -d "${FNM_DIR}" ]]; then
+#     export PATH="${FNM_DIR}:${PATH}"
+#   fi
+# fi
 
-  # === NPM BINARY CHINA ===
-  # https://github.com/cnpm/binary-mirror-config/blob/master/package.json#L48
-  # export NODE_MIRROR="https://cdn.npmmirror.com/binaries/node"
-  export NODE_MIRROR="https://mirrors.cernet.edu.cn/nodejs-release/"
-  export NVM_NODEJS_ORG_MIRROR="${NODE_MIRROR}"
-  export FNM_NODE_DIST_MIRROR="${NODE_MIRROR}"
+# if [[ -s $(command -v fnm) ]]; then
+#   # eval "$(fnm env)"
+#   # eval "$(fnm env --use-on-cd)"
+#   # completions are registered by brew already
+#   #source <(fnm completions --shell ${SHELL_NAME})
 
-  export PHANTOMJS_CDNURL="https://cdn.npmmirror.com/binaries/phantomjs"
-  export CHROMEDRIVER_CDNURL="https://cdn.npmmirror.com/binaries/chromedriver"
-  export OPERADRIVER_CDNURL="https://cdn.npmmirror.com/binaries/operadriver"
-  export ELECTRON_MIRROR="https://cdn.npmmirror.com/binaries/electron/"
-  export ELECTRON_BUILDER_BINARIES_MIRROR="https://cdn.npmmirror.com/binaries/electron-builder-binaries/"
-  export SASS_BINARY_SITE="https://cdn.npmmirror.com/binaries/node-sass"
-  export SWC_BINARY_SITE="https://cdn.npmmirror.com/binaries/node-swc"
-  export NWJS_URLBASE="https://cdn.npmmirror.com/binaries/nwjs/v"
-  export PUPPETEER_DOWNLOAD_HOST="https://cdn.npmmirror.com/binaries"
-  export SENTRYCLI_CDNURL="https://cdn.npmmirror.com/binaries/sentry-cli"
-  export SAUCECTL_INSTALL_BINARY_MIRROR="https://cdn.npmmirror.com/binaries/saucectl"
-  export npm_config_sharp_binary_host="https://cdn.npmmirror.com/binaries/sharp"
-  export npm_config_sharp_libvips_binary_host="https://cdn.npmmirror.com/binaries/sharp-libvips"
-  export npm_config_robotjs_binary_host="https://cdn.npmmirror.com/binaries/robotj"
-fi
+#   # === NPM BINARY CHINA ===
+#   # https://github.com/cnpm/binary-mirror-config/blob/master/package.json#L48
+#   # export NODE_MIRROR="https://cdn.npmmirror.com/binaries/node"
+#   export NODE_MIRROR="https://mirrors.cernet.edu.cn/nodejs-release/"
+#   export NVM_NODEJS_ORG_MIRROR="${NODE_MIRROR}"
+#   export FNM_NODE_DIST_MIRROR="${NODE_MIRROR}"
+
+#   export PHANTOMJS_CDNURL="https://cdn.npmmirror.com/binaries/phantomjs"
+#   export CHROMEDRIVER_CDNURL="https://cdn.npmmirror.com/binaries/chromedriver"
+#   export OPERADRIVER_CDNURL="https://cdn.npmmirror.com/binaries/operadriver"
+#   export ELECTRON_MIRROR="https://cdn.npmmirror.com/binaries/electron/"
+#   export ELECTRON_BUILDER_BINARIES_MIRROR="https://cdn.npmmirror.com/binaries/electron-builder-binaries/"
+#   export SASS_BINARY_SITE="https://cdn.npmmirror.com/binaries/node-sass"
+#   export SWC_BINARY_SITE="https://cdn.npmmirror.com/binaries/node-swc"
+#   export NWJS_URLBASE="https://cdn.npmmirror.com/binaries/nwjs/v"
+#   export PUPPETEER_DOWNLOAD_HOST="https://cdn.npmmirror.com/binaries"
+#   export SENTRYCLI_CDNURL="https://cdn.npmmirror.com/binaries/sentry-cli"
+#   export SAUCECTL_INSTALL_BINARY_MIRROR="https://cdn.npmmirror.com/binaries/saucectl"
+#   export npm_config_sharp_binary_host="https://cdn.npmmirror.com/binaries/sharp"
+#   export npm_config_sharp_libvips_binary_host="https://cdn.npmmirror.com/binaries/sharp-libvips"
+#   export npm_config_robotjs_binary_host="https://cdn.npmmirror.com/binaries/robotj"
+# fi
 
 # Deno
 if [[ -s "${HOME}/.deno/bin/deno" ]]; then
